78be0a44611dfe4cf845065a52b3be5ad6e14030
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2024-05-27 09:46:44 +0100

    web token added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,14 @@
+const jwt = require('jsonwebtoken');
+
 exports.login = (req, res) => {
-    const { username, password } = req.body;
-    if (username === 'SoftAmos' && password === 'SoftAmos') {
-      res.status(200).send({ message: 'Login successful' });
-    } else {
-      res.status(401).send({ message: 'Invalid credentials' });
-    }
-  };
-  
+  const { username, password } = req.body;
+  if (username === 'SoftAmos' && password === 'SoftAmos') {
+    // Assuming a secret key for JWT signing
+    const secretKey = 'your_secret_key';
+    // Generate a JWT token
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' }); // Expires in 1 hour
+    res.status(200).send({ message: 'Login successful', token });
+  } else {
+    res.status(401).send({ message: 'Invalid credentials' });
+  }
+};
