a85f45cd86f2cce23dbe626745f7fd8bb9031e80
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2024-05-27 11:00:37 +0100

    printing

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,12 +4,20 @@ const jwt = require('jsonwebtoken');
 const authenticateUser = (req, res) => {
   const { username, password } = req.body;
 
+  // Log input
+  console.log('Received username:', username);
+  console.log('Received password:', password);
+
   // Check if username and password match
   if (username === 'SoftAmos' && password === 'SoftAmos') {
     // Assuming a secret key for JWT signing
     const secretKey = 'secret_key';
     // Generate a JWT token
     const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' }); // Expires in 1 hour
+
+    // Log token
+    console.log('Generated token:', token);
+
     res.status(200).json({ message: 'Login successful', token });
   } else {
     res.status(401).json({ message: 'Authentication failed. Invalid username or password.' });
